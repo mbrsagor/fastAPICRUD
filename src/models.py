6cd033f6e8d61595bd2f7e37a67d66c6cf6c01bd
mbrsagor <mbrsagor@gmail.com>
2023-02-28 09:53:23 +0600

    New: making notes API endpoint.

--- src/models.py
+++ src/models.py
@@ -1,18 +1,15 @@
 from .database import Base
-from sqlalchemy import TIMESTAMP, Column, String, Boolean
 from sqlalchemy.sql import func
+from sqlalchemy import TIMESTAMP, Column, String, Boolean
 from fastapi_utils.guid_type import GUID, GUID_SERVER_DEFAULT_POSTGRESQL
 
 
 class Note(Base):
     __tablename__ = 'notes'
-    id = Column(GUID, primary_key=True,
-                server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
+    id = Column(GUID, primary_key=True, server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     category = Column(String, nullable=True)
     published = Column(Boolean, nullable=False, server_default='True')
-    createdAt = Column(TIMESTAMP(timezone=True),
-                       nullable=False, server_default=func.now())
-    updatedAt = Column(TIMESTAMP(timezone=True),
-                       default=None, onupdate=func.now())
+    createdAt = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    updatedAt = Column(TIMESTAMP(timezone=True), default=None, onupdate=func.now())
