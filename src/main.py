548f6f20153d6d083eaec470520a97e59c7fc719
mbrsagor <mbrsagor@gmail.com>
2023-02-28 09:35:44 +0600

    New: making notes API endpoint.

--- src/main.py
+++ src/main.py
@@ -1,35 +1,25 @@
-from fastapi import FastAPI, APIRouter, status
+from src import models, note
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-router = APIRouter()
 
+origins = [
+    "http://localhost:3000",
+]
 
-@router.get('/')
-def get_notes():
-    return "return a list of note items"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-@router.post('/', status_code=status.HTTP_201_CREATED)
-def create_note():
-    return "create note item"
-
-
-@router.patch('/{noteId}')
-def update_note(noteId: str):
-    return f"update note item with id {noteId}"
-
-
-@router.get('/{noteId}')
-def get_note(noteId: str):
-    return f"get note item with id {noteId}"
-
-
-@router.delete('/{noteId}')
-def delete_note(noteId: str):
-    return f"delete note item with id {noteId}"
-
-
-app.include_router(router, tags=['Notes'], prefix='/api/notes')
+app.include_router(note.router, tags=['Notes'], prefix='/api/notes')
 
 
 @app.get("/api/healthchecker")
